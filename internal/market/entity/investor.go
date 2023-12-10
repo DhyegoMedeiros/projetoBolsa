449f7d3046f2544525cc370b2baeafef0a4d6167
Dhyego Medeiros <diego@lbnetwork.com.br>
2023-12-10 10:08:28 -0300

    criando funcoes responsaveis pelo investidor e posições

--- internal/market/entity/investor.go
+++ internal/market/entity/investor.go
@@ -6,7 +6,43 @@ type Investor struct {
 	AssetPossition []*InvestorAssetPossition
 }
 
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:             id,
+		AssetPossition: []*InvestorAssetPossition{},
+	}
+}
+
+func (i *Investor) AddAssetPossition(assetPossition *InvestorAssetPossition) {
+	i.AssetPossition = append(i.AssetPossition, assetPossition)
+}
+
+func (i *Investor) UpdateAssetPossition(assetID string, qtdShares int) {
+	assetPossition := i.GetAssetPossition(assetID)
+	if assetPossition == nil {
+		i.AssetPossition = append(i.AssetPossition, NewInvestorAssetPossition(assetID, qtdShares))
+	} else {
+		assetPossition.Shares = qtdShares
+	}
+}
+
+func (i *Investor) GetAssetPossition(assetID string) *InvestorAssetPossition {
+	for _, assetPossition := range i.AssetPossition {
+		if assetPossition.AssetID == assetID {
+			return assetPossition
+		}
+	}
+	return nil
+}
+
 type InvestorAssetPossition struct {
 	AssetID string
 	Shares  int
+}
+
+func NewInvestorAssetPossition(assetID string, shares int) *InvestorAssetPossition {
+	return &InvestorAssetPossition{
+		AssetID: assetID,
+		Shares:  shares,
+	}
 }
